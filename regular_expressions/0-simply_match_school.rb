aba8d69ca5d469cb4169d8a1961dd13ee12cd44c
TheePoseidon <169036596+TheePoseidon@users.noreply.github.com>
2024-09-10 11:49:27 +0300

    Task 0

--- regular_expressions/0-simply_match_school.rb
+++ regular_expressions/0-simply_match_school.rb
@@ -1,14 +1,2 @@
 #!/usr/bin/env ruby
-input = ARGV[0]
-if input == "School"
-  puts "School"
-
-elsif input == "Sthool"
-  puts input
-
-elsif input == "Neha loves School, that is why she is a School mentor :)"
-  puts input
-
-else
-  puts "Invalid input"
-end
+puts ARGV[0].scan(/School/).join
